f6de58113f6142e79ec15e2d56f13d9bb3aa2ad7
mengwox <mwh8699329@163.com>
2023-08-05 12:14:50 +0800

    add "tutorial" dir

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # python-beginner
+
 python入门教程
+
 - [廖雪峰Python教程](https://www.liaoxuefeng.com/wiki/1016959663602400)
+- [OSSU-Python For Everybody](https://www.py4e.com/)
+- [Python官方Tutorial](https://docs.python.org/3/tutorial/index.html)
+
+# python
+
+## string字符串
+
+string, 实际上是character的array.
+
+python支持索引为负数, 从-1开始计数, 表示倒数第一个
+
+```python
+word = 'Python'
+# word[0] = P, word[-1] = n
+```
+
+### Slicing切片
+
+字符串切片, 相当于java的string.split.
+
+对于一个字符串s,索引i:
+
+1. 总是有`s[:i] + s[i:] = s`
+2. `s[i:j]`的字符长度 = `j-i`
+3. 索引越界的index`j`, `s[j]`会报IndexError
+4. python string slice,会自动处理索引越界问题, 所以`s[:j]`不会报IndexError
+5. slice索引有默认值, 省略的第一个索引默认为0, 省略的第二个索引默认为the end of string
+6. string在python中,和java类似(final修饰),string in python也是不可修改(immutable-不可变对象)
---
+++ beginner/tutorial/flow-control/flow_control.py
@@ -0,0 +1,100 @@
+def abs_num(x):
+    if x < 0:
+        return -x
+    elif x == 0:
+        return x
+    else:
+        return x
+
+
+print(abs_num(-10))
+print(abs_num(0))
+print(abs_num(10))
+
+
+def http_error(http_status: int):
+    match http_status:
+        case 200:
+            print("http status is 200 ok")
+        case 400:
+            print("http status is 400 valid error")
+        case 500:
+            print("http status is 500 system error")
+        case _:
+            print("unknown http status! status: %d" % http_status)
+
+
+status = 200
+http_error(status)
+status = 400
+http_error(status)
+status = 500
+http_error(status)
+status = 404
+http_error(status)
+
+
+def pointer(pointer_point):
+    match pointer_point:
+        case (0, 0):
+            print('Origin')
+        case (x, 0):
+            print(f'X={x}')
+        case (0, y):
+            print(f'Y={y}')
+        case (x, y):
+            print(f'X={x}, Y={y}')
+        case _:
+            raise ValueError('Not a point')
+
+
+point = 1
+try:
+    pointer(point)
+except ValueError as ve:
+    print(ve.args[0])
+point = (0, 0)
+pointer(point)
+point = (0, 1)
+pointer(point)
+point = (1, 0)
+pointer(point)
+point = (1, 2)
+pointer(point)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def where_is(pointObj: Point):
+    match pointObj:
+        case Point(x=0, y=0):
+            print('Origin')
+        case Point(x=x, y=0):
+            print(f'X={x}')
+        case Point(x=0, y=y):
+            print(f'Y={y}')
+        case Point(x=x, y=y):
+            print(f'X={x}, Y={y}')
+        case _:
+            raise ValueError('Not a point')
+
+
+print("match by class Point start ==================")
+point = Point(0, 0)
+where_is(point)
+point = Point(0, 1)
+where_is(point)
+point = Point(1, 0)
+where_is(point)
+point = Point(1, 2)
+where_is(point)
+point = 1
+try:
+    # noinspection PyTypeChecker
+    where_is(point)
+except ValueError as ve:
+    print(ve.args[0])
