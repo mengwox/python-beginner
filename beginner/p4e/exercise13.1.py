a2ee6cfc23ed6e7cf78464cefc63ea83eabb1b1c
mengwox <mwh8699329@163.com>
2023-09-17 00:08:20 +0800

    rename file according to p4e code

---
+++ beginner/p4e/exercise13.1.py
@@ -0,0 +1,6 @@
+# Change the socket program socket1.py to prompt the user for the URL so it can read any web page.
+# You can use split('/') to break the URL into its component parts so you can extract the host name for
+# the socket connect call. Add error checking using try and except to handle the condition where the user
+# enters an improperly formatted or non-existent URL.
+
+
