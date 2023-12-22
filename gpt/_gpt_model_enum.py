8e42cdd4bf9021216e8d154153697f3eb51c69ce
mengwox <mwh8699329@163.com>
2023-12-22 15:14:32 +0800

    add _gpt_model_enum.py

--- gpt/_gpt_model_enum.py
+++ gpt/_gpt_model_enum.py
@@ -2,34 +2,27 @@ from enum import Enum
 
 
 class GptModelDefines(Enum):
-	class Gpt4Tubro:
-		""" Gpt-4-Tubro
-		最大支持128K的上下文, 功能比Gpt-4更强大
-		"""
+	GPT4_TUBRO_PREVIEW_1106 = "gpt-4-1106-preview"
+	""" input: $0.01 / 1K token; output: $0.03 / 1K tokens """
+	GPT4_TUBRO_VISION_PREVIEW_1106 = "gpt-4-1106-vision-preview"
+	""" input: $0.01 / 1K token; output: $0.03 / 1K tokens """
+	# gpt4-tubro end
 
-		preview_1106 = "gpt-4-1106-preview"
-		""" input: $0.01 / 1K token; output: $0.03 / 1K tokens """
-		vision_preview_1106 = "gpt-4-1106-vision-preview"
-		""" input: $0.01 / 1K token; output: $0.03 / 1K tokens """
+	GPT4 = "gpt-4"
+	""" input: $0.03 / 1K token; output: $0.06 / 1K tokens """
+	GPT4_32K = "gpt4-32k"
+	""" input: $0.06 / 1K token; output: $0.12 / 1K tokens """
+	# gpt4 end
 
-	class Gpt4:
-		""" Gpt-4
-		不如Gpt-4-Tubro, 不推荐使用
-		"""
-
-		gpt4 = "gpt-4"
-		""" input: $0.03 / 1K token; output: $0.06 / 1K tokens """
-		gpt4Large = "gpt4-32k"
-		""" input: $0.06 / 1K token; output: $0.12 / 1K tokens """
-
-	class Gpt3Point5Tubro:
-		gpt = "gpt-3.5-turbo-1106"
-		""" 最大支持16K上下文 
-		input: $0.0010 / 1K token; output: $0.002 / 1K tokens """
-
-		gpt4Large = "gpt-3.5-turbo-instruct"
-		""" 最大支持4K上下文, 模型已较为过时
-		input: $0.0015 / 1K token; output: $0.002 / 1K tokens """
+	GPT3_5_TURBO = "gpt-3.5-turbo"
+	"""普通"""
+	GPT3_5_TUBRO_1106 = "gpt-3.5-turbo-1106"
+	""" 最大支持16K上下文 
+	input: $0.0010 / 1K token; output: $0.002 / 1K tokens """
+	GPT_3_5_TUBO_INSTRUCT = "gpt-3.5-turbo-instruct"
+	""" 最大支持4K上下文, 模型已较为过时
+	input: $0.0015 / 1K token; output: $0.002 / 1K tokens """
+# gpt3.5-tubro end
 
 # Assistants API 助手API
 # Fine-tuning models 自定义微调模型
